ccdea1019b30b3d6cb7c41fb2f61f35e2f16c25e
cnarbais1 <cnarbais1@mail.austral.edu.ar>
2025-03-28 12:54:19 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -5,4 +5,3 @@ def ada():
     print(first_name.capitalize(), last_name.capitalize())
     print(first_name.upper(), last_name.upper())
     print(f"\t{first_name.lower()} {last_name.lower()}")
-ada()
